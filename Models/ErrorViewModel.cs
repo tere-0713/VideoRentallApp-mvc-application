99276bcc8d3bbcb4ea0ca6423a2e207f1edafc76
Paulo <Paulo@DESKTOP-32TVN83>
2023-02-10 09:59:18 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VideoRentallApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
